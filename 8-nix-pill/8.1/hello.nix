68c00e2e4971350a7c17d52f12fc71c105a7d189
victor Hernandez <victor.hernbar@gmail.com>
2020-09-17 19:51:58 -0500

    full 8

--- 8-nix-pill/8.1/hello.nix
+++ 8-nix-pill/8.1/hello.nix
@@ -5,6 +5,7 @@ derivation {
   args = [ ./hello_builder.sh ];
   inherit gnutar gzip gnumake gcc coreutils gawk gnused gnugrep;
   binutils = binutils-unwrapped;
+  bintools = binutils.bintools;
   src = ./hello-2.10.tar.gz;
   system = builtins.currentSystem;
 }
--- 8-nix-pill/8.1/hello_builder.sh
+++ 8-nix-pill/8.1/hello_builder.sh
@@ -1,4 +1,8 @@
-export PATH="$gnutar/bin:$gcc/bin:$gnumake/bin:$coreutils/bin:$gawk/bin:$gzip/bin:$gnugrep/bin:$gnused/bin:$builtins/bin"
+export PATH="$gnutar/bin:$gcc/bin:$gnumake/bin:$coreutils/bin:$gawk/bin:$gzip/bin:$gnugrep/bin:$gnused/bin:$builtins/bin:$bintools/bin"
+echo "PATH=$PATH"
+echo "binutils=$binutils"
+echo "bintools=$bintools"
+echo "binutils_unwrapped=$binutils_unwrapped"
 tar -xzf $src
 cd hello-2.10
 ./configure --prefix=$out
